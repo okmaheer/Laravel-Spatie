be3d77d9a51a4d2d6f8da8234ad6aced479de67a
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-12-27 00:39:49 +0500

    changes

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -19,8 +19,15 @@ class AdminSeeder extends Seeder
         User::create([
             'name' => 'admin',
             'email' => 'admin@admin.com',
-            'email_verified_at' => now(),
+            'email_verified_at' => now(), ///password = password
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ])->assignRole('writer', 'admin');
+        
+        User::create([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ])->assignRole('user');
     }
 }
